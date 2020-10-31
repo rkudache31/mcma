7e1faafea61245a403fe013999d4d19ccc54ab40
rkudache31 <30896559+rkudache31@users.noreply.github.com>
2020-10-31 18:29:08 +0530

    Add files via upload

--- install_mcmaapp.sh
+++ install_mcmaapp.sh
@@ -3,18 +3,19 @@ mkdir mcmacodedir
 cd  mcmacodedir
 git clone https://github.com/rkudache31/mcma.git
 
-apt-get install unzip
+apt-get install -y unzip
 unzip mcmacode.zip
 cd mcma
+
 sudo get -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-sudo apt-get install gnupg
+sudo apt-get install -y gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64,s390x ] http://repo.mongodb.com/apt/ubuntu bionic/mongodb-enterprise/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
 sudo apt-get update
 sudo apt-get install -y mongodb-enterprise
 sudo apt install -y nodejs
 sudo apt  install  -y npm
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 sudo pip3 install pymongo
 sudo pip3 install flask
 sudo pip3 install flask_cors
