a0d7ca5c952faff3df072a82ca6f43291542b157
rkudache31 <30896559+rkudache31@users.noreply.github.com>
2020-11-01 12:18:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ else
 sudo service  mongod start
 cd /home/mcmaadmin/mcma/mCMA-codebase/mcma_backend
 nohup python3 /home/mcmaadmin/mcma/mCMA-codebase/mcma_backend/mcma_flask.py &
-sed -i 's/18.191.158.237/$1/g' /home/mcmaadmin/mcma/mCMA-codebase/mcma-UI/src/environments/environment.prod.ts
-sed -i 's/18.219.223.111/$1/g' /home/mcmaadmin/mcma/mCMA-codebase/mcma-UI/src/environments/environment.ts
-sed -i 's/ng serve/ng serve --host 0.0.0.0/g' /home/mcmaadmin/mcma/mCMA-codebase/mcma-UI/package.json
+sed -i 's/18.191.158.237/$1/g' /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma-UI/src/environments/environment.prod.ts
+sed -i 's/18.219.223.111/$1/g' /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma-UI/src/environments/environment.ts
+sed -i 's/ng serve/ng serve --host 0.0.0.0/g' /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma-UI/package.json
 
 cd /home/mcmaadmin/mcma/mCMA-codebase/mcma-UI
 npm install
