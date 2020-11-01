e39e3133ebde83fdcd6a5e172a9921468a98303a
rkudache31 <30896559+rkudache31@users.noreply.github.com>
2020-11-01 11:54:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ if [ -z $1 ]
 then
 echo "Please provide public ipaddres as argument E:g sh start.sh 104.211.118.154"
 else
+sudo service  mongod start
 cd /home/mcmaadmin/mcma/mCMA-codebase/mcma_backend
 nohup python3 /home/mcmaadmin/mcma/mCMA-codebase/mcma_backend/mcma_flask.py &
 sed -i 's/18.191.158.237/$1/g' /home/mcmaadmin/mcma/mCMA-codebase/mcma-UI/src/environments/environment.prod.ts
