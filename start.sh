6b71bd6aec423d2473475bc0c8087abb58f9a91c
rkudache31 <30896559+rkudache31@users.noreply.github.com>
2020-11-01 12:21:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@ then
 echo "Please provide public ipaddres as argument E:g sh start.sh 104.211.118.154"
 else
 sudo service  mongod start
-cd /home/mcmaadmin/mcma/mCMA-codebase/mcma_backend
-nohup python3 /home/mcmaadmin/mcma/mCMA-codebase/mcma_backend/mcma_flask.py &
+cd /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma_backend
+nohup python3 /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma_backend/mcma_flask.py &
 sed -i 's/18.191.158.237/$1/g' /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma-UI/src/environments/environment.prod.ts
 sed -i 's/18.219.223.111/$1/g' /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma-UI/src/environments/environment.ts
 sed -i 's/ng serve/ng serve --host 0.0.0.0/g' /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma-UI/package.json
 
-cd /home/mcmaadmin/mcma/mCMA-codebase/mcma-UI
+cd /var/lib/waagent/custom-script/download/0/mcma/mCMA-codebase/mcma-UI
 npm install
 npm start
 fi
